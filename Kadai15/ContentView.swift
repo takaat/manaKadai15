d0d63b0dc7e172449576110ee8504a36813daa9e
mana.u <great8life@gmail.com>
2022-01-12 20:47:12 +0900

    20220112_2046

--- Kadai15/ContentView.swift
+++ Kadai15/ContentView.swift
@@ -7,15 +7,119 @@
 
 import SwiftUI
 
+struct Item: Identifiable {
+    let id = UUID()
+    let name: String
+    var isChecked: Bool
+}
+
 struct ContentView: View {
+    @State private var checkMark = Image(systemName: "checkmark")
+    @State private var isShowAddItemView = false
+    @State private var items: [Item] = [.init(name: "りんご", isChecked: false),
+                                        .init(name: "みかん", isChecked: true),
+                                        .init(name: "バナナ", isChecked: false),
+                                        .init(name: "パイナップル", isChecked: true)]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(items) { item in
+                var item = item
+                HStack {
+                    if item.isChecked {
+                        checkMark.foregroundColor(.orange)
+                    } else {
+                        checkMark.hidden()
+                    }
+
+                    Button(action: {
+                        item.isChecked.toggle()
+                        print("ボタン作動：イズチェックは、\(item.isChecked)")
+                    }, label: {
+                        Text(item.name)
+                    })
+                }
+            }
+            .listStyle(.plain)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: { isShowAddItemView = true },
+                           label: { Image(systemName: "plus") })
+                }
+            }
+        }
+        .fullScreenCover(isPresented: $isShowAddItemView) {
+            AddItemView(isShowView: $isShowAddItemView) {
+                items.append($0)
+            }
+        }
+    }
+}
+
+struct AddItemView: View {
+    @Binding var isShowView: Bool
+    @State private var name = ""
+    let didSave: (Item) -> Void
+
+    var body: some View {
+        NavigationView {
+            HStack(spacing: 30) {
+                Text("名前")
+                    .padding(.leading)
+
+                TextField("", text: $name)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.trailing)
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        isShowView = false
+                    }
+                }
+
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Save") {
+                        didSave(.init(name: name, isChecked: false))
+                        isShowView = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ItemView: View {
+    let item: Item
+    private let checkMark = Image(systemName: "checkmark")
+
+    var body: some View {
+        HStack {
+            if item.isChecked {
+                checkMark.foregroundColor(.orange)
+            } else {
+                checkMark.hidden()
+            }
+
+            Text(item.name)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct AddItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddItemView(isShowView: .constant(true), didSave: { _ in  })
+    }
+}
+
+struct ItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemView(item: .init(name: "みかん", isChecked: true))
     }
 }
